2423adac2aadb7c3ac91a18a1022457144a85a21
Erik Baliov <vfxbaliof@gmail.com>
2018-03-13 16:26:12 +0200

    C# Conditional Statements And Loops Lab

---
+++ C# CONDITIONAL STATEMENTS AND LOOPS/09. Multiplication Table/09. Multiplication Table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteeger = int.Parse(Console.ReadLine());
+            int times = 1;
+            int result = 0;
+            do
+            {
+                result = theInteeger * times;
+
+                Console.WriteLine($"{theInteeger} X {times} = {result}");
+
+                times++;
+            } while (times <= 10);
+        }
+    }
+}
---
+++ C# CONDITIONAL STATEMENTS AND LOOPS/10. Multiplication Table 2.0/10. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int theInteeger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            int result = 0;
+            do
+            {
+                result = theInteeger * times;
+
+                Console.WriteLine($"{theInteeger} X {times} = {result}");
+
+                times++;
+            } while (times <= 10);
+
+
+
+        }
+    }
+}
