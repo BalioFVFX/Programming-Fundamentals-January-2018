ccf169488fac05988eca1115c2a9846825b1db2c
Erik Baliov <vfxbaliof@gmail.com>
2018-03-13 16:27:57 +0200

    C# Intro And Basic Syntax Lab

---
+++ C# INTRO AND BASIC SYNTAX/02. Add Two Numbers/02. Add Two Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, firstNumber + secondNumber);
+
+        }
+    }
+}
---
+++ C# INTRO AND BASIC SYNTAX/03. Employee Data/03. Employee Data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Name: {age}");
+            Console.WriteLine($"Name: {employeeID:d8}");
+            Console.WriteLine($"Name: {salary:f2}");
+        }
+    }
+}
