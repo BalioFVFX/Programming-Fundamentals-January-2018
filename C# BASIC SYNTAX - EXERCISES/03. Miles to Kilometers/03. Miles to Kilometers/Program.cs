769692a11f2290e35f38db0e504fb58271fd93e0
Erik Baliov <vfxbaliof@gmail.com>
2018-03-13 16:25:20 +0200

    C# Basic Syntax Exercises

---
+++ C# BASIC SYNTAX - EXERCISES/03. Miles to Kilometers/03. Miles to Kilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometer = 1.60934;
+
+            double miles = double.Parse(Console.ReadLine());
+
+            double kilometers = kilometer * miles;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
---
+++ C# BASIC SYNTAX - EXERCISES/04. Beverage Labels/04. Beverage Labels/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContentPer100ml = int.Parse(Console.ReadLine());
+            int sugarContentPer100ml = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{(double)energyContentPer100ml * volume / 100}kcal, {(double)sugarContentPer100ml * volume / 100}g sugars");
+        }
+    }
+}
